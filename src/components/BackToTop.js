db2d6b55528d13dd982a2a3408e73c64f0f92990
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2021-04-21 16:37:13 +0530

    cleared all files for new site

--- src/components/BackToTop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function BackToTop() {
-    return (
-        <div>
-            <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
-        </div>
-    )
-}
--- src/components/NotFound404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NotFound404() {
-    return (
-        <section>
-            <h1>404 Not found :{"("}</h1>
-        </section>
-    )
-}
